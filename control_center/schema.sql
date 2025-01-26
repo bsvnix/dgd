2eeb6074ec5baffb2cff2d93bb500532264decbd
bsvnix <111151951+bsvnix@users.noreply.github.com>
2025-01-26 00:40:31 -0700

    Update schema.sql

--- control_center/schema.sql
+++ control_center/schema.sql
@@ -1,11 +1,9 @@
--- Table for storing scan results
 CREATE TABLE IF NOT EXISTS scan_results (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     ip TEXT NOT NULL,
     open_ports TEXT NOT NULL
 );
 
--- Table for storing decoy events
 CREATE TABLE IF NOT EXISTS decoy_events (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     decoy_name TEXT NOT NULL,
